71aec3dd25f6a9204f6772ae3906f6931b35bd81
yahomi-jp <estone111223@gmail.com>
2021-09-22 22:24:25 +0900

    create hand-model

--- janken/models.py
+++ janken/models.py
@@ -6,10 +6,51 @@ from django.conf import settings
 
 class Opponent(models.Model):
     name = models.CharField('対戦相手', max_length=12)
-    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='作成者', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        verbose_name='作成者',
+        on_delete=models.CASCADE
+    )
 
     class Meta:
         db_table = 'Opponents'
-    
+
     def __str__(self):
         return self.name
+
+class Match(models.Model):
+    HANDS = (
+        ('rock', 'グー'),
+        ('paper', 'チョキ'),
+        ('scissors', 'パー'),
+    )
+    RESULTS = (
+        ('win', '勝ち'),
+        ('lose', '負け'),
+        ('draw', 'あいこ'),
+    )
+    opponent = models.ForeignKey(
+        Opponent,
+        verbose_name='対戦相手',
+        on_delete=models.CASCADE
+    )
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        verbose_name='作成者',
+        on_delete=models.CASCADE
+    )
+    oppnent_hand = models.CharField(
+        '相手の手',
+        max_length=1,
+        choices=HANDS
+    )
+    my_hand = models.CharField(
+        '自分の手',
+        max_length=1,
+        choices=HANDS
+    )
+    result = models.CharField(
+        '対戦結果',
+        max_length=4,
+        choices=RESULTS
+    )
