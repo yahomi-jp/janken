6c7f0632de981c97d56d401b00239bca1d1f59d9
yahomi-jp <estone111223@gmail.com>
2021-09-28 01:40:45 +0900

    [update] game_resultを実装

--- janken/urls.py
+++ janken/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.top, name='top'),
     path('<int:opponent_id>/', views.opponent_detail, name='op_detail'),
     path('new/', views.opponent_new, name='op_new'),
+    path('<int:opponent_id>/register/', views.game_register, name='game_register'),
 ]
--- janken/views.py
+++ janken/views.py
@@ -52,8 +52,16 @@ def game_register(request, opponent_id):
             form = GameForm(request.POST)
             if form.is_valid():
                 game = form.save(commit=False)
-                game.opponent = opponent_id
+                game.opponent_id = opponent_id
                 game.created_by = request.user
-                
+                # 勝敗判定
+                if int(game.my_hand) - int(game.opponent_hand) + 3 % 3 == 2:
+                    game.result = '勝ち'
+                elif int(game.my_hand) - int(game.opponent_hand) + 3 % 3 == 1:
+                    game.result = '負け'
+                else:
+                    game.result = 'あいこ'
+                game.save()
+                return redirect('janken:op_detail', opponent_id)
         else:
             return HttpResponseForbidden('正規の手順を踏んでください')
